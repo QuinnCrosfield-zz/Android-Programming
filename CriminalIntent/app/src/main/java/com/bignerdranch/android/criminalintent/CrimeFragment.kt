23c624dd5f6e34b5e36249cf6caf6e9b7c592d06
QuinnCrosfield <qcrosfie@gustavus.edu>
2020-04-05 18:57:41 -0500

    inflate the fragment's view

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
@@ -1,6 +1,9 @@
 package com.bignerdranch.android.criminalintent
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class CrimeFragment : Fragment() {
@@ -10,6 +13,15 @@ class CrimeFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         crime = Crime()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime, container, false)
+        return view
     }
 
 }
