71d1c3669bb2d998d33d4c590cce91842bfd5310
QuinnCrosfield <qcrosfie@gustavus.edu>
2020-04-05 18:53:58 -0500

    override onCreate

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
@@ -1,7 +1,15 @@
 package com.bignerdranch.android.criminalintent
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 
 class CrimeFragment : Fragment() {
+
+    private lateinit var crime: Crime
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        crime = Crime()
+    }
 
 }
