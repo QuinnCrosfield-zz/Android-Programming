23227f6dda01a74ebd33e412f005fa7bf18f9d5e
QuinnCrosfield <qcrosfie@gustavus.edu>
2020-04-05 20:45:30 -0500

    add CrimeListFragment

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.kt
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.fragment_container)
 
         if (currentFragment == null) {
-            val fragment = CrimeFragment()
+            val fragment = CrimeListFragment.newInstance()
             supportFragmentManager
                 .beginTransaction()
                 .add(R.id.fragment_container, fragment)
@@ -21,7 +21,5 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
-
-
 
 }
