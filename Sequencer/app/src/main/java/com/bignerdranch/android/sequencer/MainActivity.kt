00b483e640df65f95928fc078cafa3bf29ba6b8b
QuinnCrosfield <qcrosfie@gustavus.edu>
2020-05-03 12:07:46 -0500

    wire bpm_view and seekbar

--- Sequencer/app/src/main/java/com/bignerdranch/android/sequencer/MainActivity.kt
+++ Sequencer/app/src/main/java/com/bignerdranch/android/sequencer/MainActivity.kt
@@ -2,11 +2,14 @@ package com.bignerdranch.android.sequencer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ProgressBar
+import android.widget.SeekBar
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var bpmView: TextView
+    private lateinit var seekBar: SeekBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +17,23 @@ class MainActivity : AppCompatActivity() {
 
         // retrieve references
         bpmView = findViewById(R.id.bpm_view)
+        seekBar = findViewById(R.id.seek_bar)
+
+        seekBar?.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+                // write custom code for progress is changed
+                bpmView.text = "" + progress
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+                // write custom code for progress is started
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+                // write custom code for progress is stopped
+            }
+        })
     }
 
 
